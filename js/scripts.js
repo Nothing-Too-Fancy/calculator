c39fd9e7c5d470aede541d2d028b07ae1567b125
Ed Finlaw <edrfinlaw@gmail.com>
2021-04-08 12:35:41 -0700

    implemented event listeners for calculator buttons (numbers, operators, clear, equals)

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
-let numberValue = 0;
+let numberValue1 = NaN;
+let numberValue2 = NaN;
 let operatorValue = '';
+
+initialize();
 
 function add(a, b) {
    return a + b;
@@ -18,20 +21,88 @@ function divide(a, b){
 }
 
 function operate(operator, num1, num2){
+   let finalValue = 0;
    switch (operator) {
       case '+':
-         return add(num1, num2);
+         finalValue = add(num1, num2);
+         break;
       case '-':
-         return subtract(num1, num2);
+         finalValue = subtract(num1, num2);
+         break;
       case '*':
-         return multiply(num1, num2);
+         finalValue = multiply(num1, num2);
+         break;
       case '/':
-         return divide(num1, num2);
+         finalValue = divide(num1, num2);
+         break;
    };
+   numberValue1 = finalValue;
+   return finalValue;
 }
 
 function displayValue(value){
    const display = document.querySelector('.display-content');
    display.textContent = value;
-   numberValue = value;
+}
+
+function initialize(){
+   const btns = document.querySelectorAll('.btn');
+   const btnArray = Array.from(btns);
+
+   for(const btn of btnArray){
+      switch(btn.textContent){
+         case '*':
+         case '/':
+         case '+':
+         case '-':
+            btn.addEventListener('click', () => {
+               if (!isNaN(numberValue1) && !isNaN(numberValue2)){
+                  displayValue(operate(operatorValue, numberValue1, numberValue2));
+               }
+               operatorValue = btn.textContent;
+            });
+         break;
+         case '1':
+         case '2':
+         case '3': 
+         case '4': 
+         case '5': 
+         case '6': 
+         case '7': 
+         case '8': 
+         case '9': 
+         case '0':
+            btn.addEventListener('click', () => {
+               if(operatorValue != ''){
+                  numberValue2 = parseInt(btn.textContent);
+               }
+               else {
+               numberValue1 = parseInt(btn.textContent);
+               }
+               displayValue(btn.textContent);
+            });
+         break;
+         case 'C':
+         case 'CE':
+            btn.addEventListener('click', () => {
+               displayValue(0);
+               numberValue1 = NaN;
+               numberValue2 = NaN;
+               operatorValue = '';
+            });
+         break;
+         case '=':
+            btn.addEventListener('click', () => {
+               if(operatorValue != '' && !isNaN(numberValue1)){
+                  if(isNaN(numberValue2)){
+                     numberValue2 = numberValue1;
+                     displayValue(operate(operatorValue, numberValue1, numberValue2));
+                  }
+                  else{
+                     displayValue(operate(operatorValue, numberValue1, numberValue2));
+                  }
+               }
+            });
+      }
+   }
 }
